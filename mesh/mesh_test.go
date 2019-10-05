2bcca303e4087e1a4d1d6c534574be6efc90bb75
Ivo Sequeros <ivosequeros@me.com>
2019-10-05 15:42:16 +0200

    Added new benchmark

--- mesh/mesh_test.go
+++ mesh/mesh_test.go
@@ -3,30 +3,14 @@ package mesh
 import "testing"
 
 func BenchmarkBroadcastWith2Peers(b *testing.B) {
-	m1 := New()
-	_ = New()
-
 	type Message map[string]interface{}
 
-	for i := 0; i < b.N; i++ {
-		m1.Broadcast("test", &Message{
-			"value": "test",
-		})
-	}
-}
-
-func BenchmarkBroadcastWith5Peers(b *testing.B) {
-	m1 := New()
-	_ = New()
-	_ = New()
-	_ = New()
-	_ = New()
-
-	type Message map[string]interface{}
-
-	for i := 0; i < b.N; i++ {
-		m1.Broadcast("test", &Message{
-			"value": "test",
-		})
-	}
+	b.RunParallel(func(pb *testing.PB) {
+		m := New()
+		for pb.Next() {
+			m.Broadcast("test", &Message{
+				"value": "test",
+			})
+		}
+	})
 }
